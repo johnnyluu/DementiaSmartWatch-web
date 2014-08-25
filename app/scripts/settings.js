a8585ac769b12cf95e8920e39808bd8437c73a23
Zeqing Lu <zeqing.lu@connect.qut.edu.au>
2014-08-25 14:57:28 +1000

    AN-9
    Improved setting fence procedure, visualise new fences on the map. Pair
    programmed with Syed and Eric.

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -1,18 +1,43 @@
 "use strict";
 var map;
+var cordx = 0.00;
+var cordy = 0.00;
+var setFence = false;
+
+function getLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else{
+		x.innerHTML = "Geolocation is not supported by your browser"
+	}
+}
+
+function showPosition(position){
+	cordx = position.coords.latitude;
+	cordy = position.coords.longitude;
+	console.log(cordx,cordy);
+	initialize();
+}
 
 function initialize() {
+	getLocation();
+
 	var mapOptions = {
-	  center: new google.maps.LatLng(-34.397, 150.644),
-	  zoom: 8
+	  center: new google.maps.LatLng(cordx, cordy),
+	  zoom: 14
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	google.maps.event.addListener(map, 'click', newFence);
 }
+
+$('#createNew').click(function(){
+	setFence = true;
+});
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function newFence(a){
-	console.log(a);
+	if(setFence==true){console.log(a);
 	var centerlat = a.latLng.B;
 	var centerlong = a.latLng.k;
 	$.post('http://localhost/save.php', {
@@ -22,4 +47,22 @@ function newFence(a){
 		console.log(data);
 	});
 	console.log(centerlat, centerlong);
+	setFence = false;
+    
+   
+    var geofence = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 1,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: a.latLng,
+      radius: 100
+    };
+    // Add the circle for this city to the map.
+    var newGeoFence = new google.maps.Circle(geofence);
+    newGeoFence.setMap(map);
+};
+	
 }
