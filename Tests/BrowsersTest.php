ee53a4f8fbb4cc3f475def1d150460eb36df11c8
Zahran AlAbri <zzalabri@gmail.com>
2014-09-08 17:24:00 +1000

    Browser change site

--- Tests/BrowsersTest.php
+++ Tests/BrowsersTest.php
@@ -1,29 +1,33 @@
 <?php
 require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+require_once 'org.openqa.selenium.server.SeleniumServer';
 
 class WebTest extends PHPUnit_Extensions_SeleniumTestCase
 {
 public static $browsers = array(
-      array(
-        'browser' => '*firefox',
-      ),
-      array(
-        'browser' => '*chrome',
-      ),
+      //array(
+      //  'browser' => '*firefox',
+      //),
+      //array(
+      //  'browser' => '*chrome',
+      //),
 	  array(
         'browser' => '*iexplore',
       )
 	  );
     protected function setUp()
     {
+		SeleniumServer seleniumserver = new SeleniumServer();
+        seleniumserver.boot();
+        seleniumserver.start();
         //$this->setBrowser('*firefox');
-        $this->setBrowserUrl('http://hungpohuang.com/agile/combined_code/');
+        $this->setBrowserUrl('http://hungpohuang.com/agile/combined_code/app/#/');
     }
 
     public function testTitle()
     {
-        $this->open('http://hungpohuang.com/agile/combined_code/');
-        $this->assertTitle('DSM');
+        $this->open('http://hungpohuang.com/agile/combined_code/app/#/');
+        $this->assertTitle('DSW');
     }
 }	
 ?>
